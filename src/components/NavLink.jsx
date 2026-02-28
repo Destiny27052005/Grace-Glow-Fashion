15f3bc20525a330604507620a08d0d3b301fdd1f
Adekunle Oluwatobiloba <146554573+Destiny27052005@users.noreply.github.com>
2026-02-28 03:20:20 +0100

    refactor NavLink component for mobile responsiveness and improved styling

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+
 function NavLink() {
+    const [isMobile, setIsMobile] = useState(false);
+
     return (
-        <ul className="flex space-x-4">
-            <li><a href="#home">HOME</a></li>
-            <li><a href="#about">ABOUT</a></li>
-            <li><a href="#services">SERVICES</a></li>
-            <li><a href="#gallery">GALLERY</a></li>
-            <li><a href="#contact">CONTACT</a></li>
-        </ul>
-    );
+        <div className="flex">
+            <ul className="hidden md:flex space-x-4">
+                <li className="text-gray-500 transition hover:text-black"><a href="#home">HOME</a></li>
+                <li className="text-gray-500 transition hover:text-black"><a href="#about">ABOUT</a></li>
+                <li className="text-gray-500 transition hover:text-black"><a href="#services">SERVICES</a></li>
+                <li className="text-gray-500 transition hover:text-black"><a href="#gallery">GALLERY</a></li>
+                <li className="text-gray-500 transition hover:text-black"><a href="#contact">CONTACT</a></li>
+            </ul>
+            <div className="md:hidden">
+                <span onClick={() => setIsMobile(!isMobile)}>
+                    <FaBars />
+                </span>
+                <ul className={`hidden ${isMobile ? "open" : ""}`}>
+                    <li className="text-gray-500 transition hover:text-black"><a href="#home">HOME</a></li>
+                    <li className="text-gray-500 transition hover:text-black"><a href="#about">ABOUT</a></li>
+                    <li className="text-gray-500 transition hover:text-black"><a href="#services">SERVICES</a></li>
+                    <li className="text-gray-500 transition hover:text-black"><a href="#gallery">GALLERY</a></li>
+                    <li className="text-gray-500 transition hover:text-black"><a href="#contact">CONTACT</a></li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default NavLink;
