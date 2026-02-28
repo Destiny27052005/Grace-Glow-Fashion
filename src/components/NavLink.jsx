872e7f0354211ccd18dab240b8b931682192004f
Adekunle Oluwatobiloba <146554573+Destiny27052005@users.noreply.github.com>
2026-02-28 03:29:02 +0100

    margin added

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -6,6 +6,7 @@ function NavLink() {
 
     return (
         <div className="flex">
+            {/* Desktop */}
             <ul className="hidden md:flex space-x-4">
                 <li className="text-gray-500 transition hover:text-black"><a href="#home">HOME</a></li>
                 <li className="text-gray-500 transition hover:text-black"><a href="#about">ABOUT</a></li>
@@ -13,16 +14,18 @@ function NavLink() {
                 <li className="text-gray-500 transition hover:text-black"><a href="#gallery">GALLERY</a></li>
                 <li className="text-gray-500 transition hover:text-black"><a href="#contact">CONTACT</a></li>
             </ul>
+
+            {/* Mobile */}
             <div className="md:hidden">
                 <span onClick={() => setIsMobile(!isMobile)}>
                     <FaBars />
                 </span>
                 <ul className={`hidden ${isMobile ? "open" : ""}`}>
-                    <li className="text-gray-500 transition hover:text-black"><a href="#home">HOME</a></li>
-                    <li className="text-gray-500 transition hover:text-black"><a href="#about">ABOUT</a></li>
-                    <li className="text-gray-500 transition hover:text-black"><a href="#services">SERVICES</a></li>
-                    <li className="text-gray-500 transition hover:text-black"><a href="#gallery">GALLERY</a></li>
-                    <li className="text-gray-500 transition hover:text-black"><a href="#contact">CONTACT</a></li>
+                    <li className="text-gray-500 transition hover:text-black mb-5"><a href="#home">HOME</a></li>
+                    <li className="text-gray-500 transition hover:text-black mb-5"><a href="#about">ABOUT</a></li>
+                    <li className="text-gray-500 transition hover:text-black mb-5"><a href="#services">SERVICES</a></li>
+                    <li className="text-gray-500 transition hover:text-black mb-5"><a href="#gallery">GALLERY</a></li>
+                    <li className="text-gray-500 transition hover:text-black mb-5"><a href="#contact">CONTACT</a></li>
                 </ul>
             </div>
         </div>
