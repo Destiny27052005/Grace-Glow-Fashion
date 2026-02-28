f47556bcf4fa2812f05c84f0e76e807e35664f98
Adekunle Oluwatobiloba <146554573+Destiny27052005@users.noreply.github.com>
2026-02-28 02:14:14 +0100

    styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { FaInstagram, FaFacebook, FaCopyright } from "react-icons/fa6";
 
 function Footer() {
     return (
-        <section className="px-12 py-16 md:flex justify-between">
+        <section className="px-12 py-16 md:flex justify-between place-items-center">
             <h1 className="sm:text-center text-3xl font-bold sm:mb-4">Serenite</h1>
             <div className="flex gap-3 sm:mb-4 sm:place-self-center">
                 <a href="http://" target="_blank" rel="noopener noreferrer"><FaInstagram className="text-2xl" /></a>
