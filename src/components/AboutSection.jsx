66a0571f6a7335a0b517d112ca893a5bfac37534
Adekunle Oluwatobiloba <146554573+Destiny27052005@users.noreply.github.com>
2026-02-28 12:02:36 +0100

    animation added

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,8 +1,38 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import AboutSpa from "../assets/images/about-spa.jpg"
 
 function AboutSection() {
+    useGSAP(() => {
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".about",
+                start: 'top 80%',
+                end: "top bottom",
+                toggleActions: "play none none reverse"
+            }
+        })
+        tl.fromTo('.about img',
+            {
+                 x: -100, opacity: 0 
+            }, {
+            x: 0,
+            opacity: 1,
+            duration: 1.2,
+ease: "power2.out"
+        })
+        tl.fromTo(".about div", {
+            x: 500,
+            opacity: 0
+        }, {
+            x: 0,
+            opacity: 1,
+              duration: 1,
+ease: "power2.out"
+        }, "-=0.5")
+    })
     return (
-        <section id="about" className="md:flex place-items-center gap-20 p-12">
+        <section id="about" className="about md:flex place-items-center gap-20 p-12 overflow-hidden">
             <img src={AboutSpa} alt="About Spa" className="md:h-150" />
             <div>
                 <h1 className="font-medium text-xl">OUR STORY</h1>
