8d1841dfd6223da143d5805fdd30b9ee36c27d7a
Adekunle Oluwatobiloba <146554573+Destiny27052005@users.noreply.github.com>
2026-02-28 02:50:52 +0100

    anchor tag  added to button

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ function HeroSection() {
     <section id="home" className="heroSection h-screen w-screen object-center text-white grid place-content-center text-center">
         <h1 className="text-6xl font-extrabold mb-6">Beauty in <br />Simplicity</h1>
         <p className="mb-6">A sanctuary of calm where modern beauty meets timeless elegence</p>
-        <button className="font-medium border-white place-self-center border w-35 px-5 py-3 transition-all hover:bg-white hover:text-black">BOOK NOW</button>
+        <button className="font-medium border-white place-self-center border w-35 px-5 py-3 transition-all hover:bg-white hover:text-black cursor-pointer"><a href="#contact">BOOK NOW</a></button>
     </section>
      );
 }
