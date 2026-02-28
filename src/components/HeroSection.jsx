2171506c71e241769836c360d6400e2cc12bf6dc
Adekunle Oluwatobiloba <146554573+Destiny27052005@users.noreply.github.com>
2026-02-28 22:53:21 +0100

    padding added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -33,7 +33,7 @@ function HeroSection() {
     )
 
     return (
-        <section id="home" className="heroSection h-screen w-screen object-center text-white grid place-content-center text-center">
+        <section id="home" className="heroSection h-screen w-screen object-center text-white grid place-content-center text-center px-12">
             <h1 className="hero-title text-6xl font-extrabold mb-6">Beauty in <br />Simplicity</h1>
             <p className="hero-text mb-6">A sanctuary of calm where modern beauty meets timeless elegence</p>
             <button className="font-medium border-white place-self-center border w-35 px-5 py-3 transition-all hover:bg-white hover:text-black cursor-pointer"><a href="#contact">BOOK NOW</a></button>
