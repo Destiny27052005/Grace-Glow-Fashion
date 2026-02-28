928a8479d4caca5ea520f5e03a1da37c541fc6c4
Adekunle Oluwatobiloba <146554573+Destiny27052005@users.noreply.github.com>
2026-02-28 01:53:01 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,16 @@
-
+import { FaInstagram, FaFacebook, FaCopyright } from "react-icons/fa6";
 
 function Footer() {
-    return ( 
-    <section></section>
-     );
+    return (
+        <section className="px-12 py-16 md:flex justify-between">
+            <h1 className="font-bold sm:mb-4">Serenite</h1>
+            <div className="flex gap-3 sm:mb-4">
+                <FaInstagram />
+                <FaFacebook />
+            </div>
+            <p className="text-gray-500"><FaCopyright /> 2026 Serenite. All rights reserved.</p>
+        </section>
+    );
 }
 
 export default Footer;
