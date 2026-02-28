c91bb99530eaf2e6a4855817eeb96aa7f28a4a13
Adekunle Oluwatobiloba <146554573+Destiny27052005@users.noreply.github.com>
2026-02-28 10:27:31 +0100

    animation added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
 import NavLink from "./NavLink";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
 
 function Navbar() {
+    useGSAP(() => {
+        gsap.to('#nav', {
+            backgroundColor: 'white',
+            scrollTrigger: {
+                trigger: 'body',
+                start: 'top -10%',
+                end: 'top -20%',
+                scrub: true,
+            }
+        });
+    });
+
     return (
-        <nav id="nav" className="flex justify-between place-items-center p-4 fixed w-full">
+        <nav id="nav" className="nav flex justify-between place-items-center p-4 fixed w-full">
             <h1 className="font-bold text-3xl">Serenite</h1>
             <NavLink />
         </nav>
