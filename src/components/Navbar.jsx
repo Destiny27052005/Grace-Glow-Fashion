3dc4cf80e66a8ffb34f40809cf8d5eec2f24170c
Adekunle Oluwatobiloba <146554573+Destiny27052005@users.noreply.github.com>
2026-02-28 03:32:19 +0100

    positin change to fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import NavLink from "./NavLink";
 
 function Navbar() {
     return (
-        <nav className="flex justify-between place-items-center p-4 absolute w-full">
+        <nav id="nav" className="flex justify-between place-items-center p-4 fixed w-full">
             <h1 className="font-bold text-3xl">Serenite</h1>
             <NavLink />
         </nav>
